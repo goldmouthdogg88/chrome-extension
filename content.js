cfd1063ca8229c9bc958a6d298c92c809ed2cb3b
goldmouthdogg88 <goldmouthdogg88@gmail.com>
2022-05-03 00:41:01 -0400

    fixed date field

--- content.js
+++ content.js
@@ -3,13 +3,14 @@
 window.onload = function () {
   var xhr = new XMLHttpRequest();
   const url = window.location.href;
+  const date = new Date();
   xhr.open("POST", `https://localhost:9000/web_history`, true);
   xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
   xhr.onload = function () {
     // do something to response
     console.log(this.responseText);
   };
-  xhr.send(`title=${document.title}&url=${url}&date=${new Date()}`);
+  xhr.send(`title=${document.title}&url=${url}&date=${date.toJSON()}`);
 };
 
 function url() {
